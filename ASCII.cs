0d430968e7159f80dc5d4a23b4b7a5df9ff3c699
339571119 <339571119@2260I-L29-010>
2023-09-20 10:58:01 -0400

    Add project files.

---
+++ ASCII.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_4._5
+{
+    internal class ASCII
+    {
+
+        
+
+        public void DrawSonic()
+        {
+            Console.WriteLine("                             ...,?77??!~~~~!???77?<~....             ");
+            Console.WriteLine("                        ..?7`                           `7!..        ");
+            Console.WriteLine("                    .,=`          ..~7^`   I                  ?1.    ");
+            Console.WriteLine("       ........  ..^            ?`  ..?7!1 .               ...??7    ");
+            Console.WriteLine("      .        .7`        .,777.. .I.    . .!          .,7!          ");
+            Console.WriteLine("      ..     .?         .^      .l   ?i. . .`       .,^              ");
+            Console.WriteLine("       b    .!        .= .?7???7~.     .>r .      .=                 ");
+            Console.WriteLine("       .,.?4         , .^         1        `     4...                ");
+            Console.WriteLine("        J   ^         ,            5       `         ?<.             ");
+            Console.WriteLine("       .%.7;         .`     .,     .;                   .=.          ");
+            Console.WriteLine("       .+^ .,       .%      MML     F       .,             ?,        ");
+            Console.WriteLine("        P   ,,      J      .MMN     F        6               4.      ");
+            Console.WriteLine("        l    d,    ,       .MMM!   .t        ..               ,,     ");
+            Console.WriteLine("        ,    JMa..`         MMM`   .         .!                .;    ");
+            Console.WriteLine("         r   .M#            .M#   .%  .      .~                 .,   ");
+            Console.WriteLine("       dMMMNJ..!                 .P7!  .>    .         .         ,,  ");
+            Console.WriteLine("       .WMMMMMm  ?^..       ..,?! ..    ..   ,  Z7`        `?^..  ,, ");
+            Console.WriteLine("          ?THB3       ?77?!        .Yr  .   .!   ?,              ?^C ");
+            Console.WriteLine("            ?,                   .,^.` .%  .^      5.                ");
+            Console.WriteLine("              7,          .....?7     .^  ,`        ?.               ");
+            Console.WriteLine("                `<.                 .= .`'           1               ");
+            Console.WriteLine("                ....dn... ... ...,7..J=!7,           .,              ");
+            Console.WriteLine("             ..=     G.,7  ..,o..  .?    J.           F              ");
+            Console.WriteLine("           .J.  .^ ,,,t  ,^        ?^.  .^  `?~.      F              ");
+            Console.WriteLine("          r %J. $    5r J             ,r.1      .=.  .%              ");
+            Console.WriteLine("          r .77=?4.    ``,     l ., 1  .. <.       4.,               ");
+            Console.WriteLine("          .$..    .X..   .n..  ., J. r .`  J.       `'               ");
+            Console.WriteLine("        .?`  .5        `` .%   .% .' L.'    t                        ");
+            Console.WriteLine("        ,. ..1JL          .,   J .$.?`      .                        ");
+            Console.WriteLine("                1.          .=` ` .J7??7<.. .;                       ");
+            Console.WriteLine("                 JS..    ..^      L        7.:                       ");
+            Console.WriteLine("                   `> ..       J.  4.                                ");
+            Console.WriteLine("                    +   r `t   r ~=..G.                              ");
+            Console.WriteLine("                    =   $  ,.  J                                     ");
+            Console.WriteLine("                    2   r   t  .;                                    ");
+            Console.WriteLine("              .,7!  r   t`7~..  j..                                  ");
+            Console.WriteLine("              j   7~L...$=.?7r   r ;?1.                              ");
+            Console.WriteLine("               8.      .=    j ..,^   ..                             ");
+            Console.WriteLine("              r        G              .                              ");
+            Console.WriteLine("            .,7,        j,           .>=.                            ");
+            Console.WriteLine("         .J??,  `T....... %             ..                           ");
+            Console.WriteLine("      ..^     <.  ~.    ,.             .D                            ");
+            Console.WriteLine("    .?`        1   L     .7.........?Ti..l                           ");
+            Console.WriteLine("   ,`           L  .    .%    .`!       `j,                          ");
+            Console.WriteLine(" .^             .  ..   .`   .^  .?7!?7+. 1                          ");
+            Console.WriteLine(".`              .  .`..`7.  .^  ,`      .i.;                         ");
+            Console.WriteLine(".7<..........~<<3?7!`    4. r  `          G%                         ");
+            Console.WriteLine("                          J.` .!           %                         ");
+            Console.WriteLine("                            JiJ           .`                         ");
+            Console.WriteLine("                              .1.         J                          ");
+            Console.WriteLine("                                 ?1.     .'                          ");
+            Console.WriteLine("                                     7<..%                           ");
+        }
+
+        public void DrawSkull()
+        {
+            Console.WriteLine("                              ,--.         ");
+            Console.WriteLine("                             {    }        ");
+            Console.WriteLine("                             K,   }        ");
+            Console.WriteLine("                            /  `Y`         ");
+            Console.WriteLine("                       _   /   /           ");
+            Console.WriteLine("                      {_'-K.__/            ");
+            Console.WriteLine("                        `/-.__L._          ");
+            Console.WriteLine("                        /  ' /`\\_}        ");
+            Console.WriteLine("               ____   /  ' /               ");
+            Console.WriteLine("        ,-'~~~~    ~~/  ' /_               ");
+            Console.WriteLine("      ,'             ``~~~%%',             ");
+            Console.WriteLine("     (                     %  Y            ");
+            Console.WriteLine("    {                      %% I            ");
+            Console.WriteLine("   {      -                 %  `.          ");
+            Console.WriteLine("   |       ',                %  )          ");
+            Console.WriteLine("   |        |   ,..__      __. Y           ");
+            Console.WriteLine("   |    .,_./  Y ' / ^Y   J   )|           ");
+            Console.WriteLine("   \\           |' /   |   |   ||          ");
+            Console.WriteLine("    \\          L_/    . _ (_,.'(          ");
+            Console.WriteLine("     \\,   ,     ^^^^' / |      )          ");
+            Console.WriteLine("       \\_  \\          /,L]     /         ");
+            Console.WriteLine("         '-_`-,       ` `   ./`            ");
+            Console.WriteLine("            `-(_            )              ");
+            Console.WriteLine("                ^^\\..___,.--`             ");
+        }
+
+        public void DrawVenusaur()
+        {
+            Console.WriteLine("                           _._       _,._ ");
+            Console.WriteLine("                        _.'   `. ' .'   _`.");
+            Console.WriteLine("                ,\"\"\"/`\"\"-.-.,/. ` V'\\-,`.,--/\"\"\".\"-..");
+            Console.WriteLine("              ,'    `...,' . ,\\-----._|     `.   /   \\");
+            Console.WriteLine("             `.            .`  -'`\"\" .._   :> `-'   `.");
+            Console.WriteLine("            ,'  ,-.  _,.-'| `..___ ,'   |'-..__   .._ L");
+            Console.WriteLine("           .    \\_ -'   `-'     ..      `.-' `.`-.'_ .|");
+            Console.WriteLine("           |   ,',-,--..  ,--../  `.  .-.    , `-.  ``.");
+            Console.WriteLine("           `.,' ,  |   |  `.  /'/,,.\\/  |    \\|   |");
+            Console.WriteLine("                `  `---'    `j   .   \\  .     '   j");
+            Console.WriteLine("              ,__`\"        ,'|`'\\_/`.'\\'        |\\-'-, _,.");
+            Console.WriteLine("       .--...`-. `-`. /    '- ..      _,    /\\ ,' .--\"'  ,'\".");
+            Console.WriteLine("     _'-\"\"-    --  _`'-.../ __ '.'`-^,_`-\"\"\"\"---....__  ' _,-`");
+            Console.WriteLine("   _.----`  _..--.'        |  \"`-..-\" __|'\"'         .\"\"-. \"\"'--.._");
+            Console.WriteLine("  /        '    /     ,  _.+-.'  ||._'   \"\"\"\". .          `     .__\\");
+            Console.WriteLine(" `---    /        /  / j'       _/|..`  -. `-`\\ \\   \\  \\   `.  \\ `-..");
+            Console.WriteLine(",\" _.-' /    /` ./  /`_|_,-\"   ','|       `. | -'`._,   L  \\ .  `.   |");
+            Console.WriteLine("`\"' /  /  / ,__...-----| _.,  ,'            `|----.._`-.|' |. .` ..  .");
+            Console.WriteLine("   /  '| /.,/   \\--.._ `-,' ,          .  '`.'  __,., '  ''``._ \\ \\`,'");
+            Console.WriteLine("  /_,'---  ,     \\`._,-` \\ //  / . \\    `._,  -`,  / / _   |   `-L -");
+            Console.WriteLine("   /       `.     ,  ..._ ' `_/ '| |\\ `._'       '-.'   `.,'     |");
+            Console.WriteLine("  '         /    /  ..   `.  `./ | ; `.'    ,\"\" ,.  `.    \\      |");
+            Console.WriteLine("   `.     ,'   ,'   | |\\  |       \"        |  ,'\\ |   \\    `    ,L");
+            Console.WriteLine("   /|`.  /    '     | `-| '                  /`-' |    L    `._/  \\");
+            Console.WriteLine("  / | .`|    |  .   `._.'                   `.__,'   .  |     |  (`");
+            Console.WriteLine(" '-\"\"-'_|    `. `.__,._____     .    _,        ____ ,-  j     \".-'\"'");
+            Console.WriteLine("        \\      `-.  \\/.    `\"--.._    _,.---'\"\"\\/  \"_,.'     /-'");
+            Console.WriteLine("         )        `-._ '-.        `--\"      _.-'.-\"\"        `.");
+            Console.WriteLine("        ./            `,. `\".._________...\"\"_.-\"`.          _j");
+            Console.WriteLine("       /_\\.__,\"\".   ,.'  \"`-...________.---\"     .\".   ,.  / \\");
+            Console.WriteLine("              \\_/\"\"\"-'                           `-'--(_,`\"`-`");
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Topic_4._5
+{
+    internal class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            ASCII art1 = new ASCII();
+            ASCII art2 = new ASCII();
+            ASCII art3 = new ASCII();
+
+            Console.WriteLine("Press ENTER for a joke");
+            Console.ReadLine();
+            Joke(numBugs);
+
+            
+
+
+
+
+
+        } 
+        public static void Joke(int numBugs)
+        {
+            Console.WriteLine(numBugs + " little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine(numBugs + " little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine(" Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine((++numBugs) + " little bugs in the code.");
+        }
+
+
+    }
+
+}
